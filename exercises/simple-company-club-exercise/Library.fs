ef6a1aad9e4f5edb12341bd80c071ad022c077a0
D. Hauge <Animcuil@gmail.com>
2020-10-26 14:29:45 +0100

    Lecture5 company club exercise revised

--- exercises/simple-company-club-exercise/Library.fs
+++ exercises/simple-company-club-exercise/Library.fs
@@ -3,27 +3,27 @@
 module club =
     type TelephoneNumber = string
     type BirthYear = int
-    type Interests = string list
+    type Interests = string Set
     type Name = string
     type MemberDescription = (TelephoneNumber * BirthYear * Interests)
-    type ClubRegister = (Name * MemberDescription) list
+    type ClubRegister = Map<Name, MemberDescription>
     type Arrangement = ((TelephoneNumber -> bool) * (BirthYear -> bool) * (Interests -> bool))
     type TargetGroup = (Name * TelephoneNumber) list
 
     let rec allExistsWithIn a b = 
         match a with
-        | [last] when List.contains last b-> true
-        | head::tail when List.contains head b-> allExistsWithIn tail b
+        | [last] when Set.contains last b-> true
+        | head::tail when Set.contains head b-> allExistsWithIn tail b
         | _ -> false
 
     let rec oneExistsWithIn a b =
         match a with
         | [] -> false
-        | [last] when List.contains last b-> true
-        | head::_ when List.contains head b -> true
+        | [last] when Set.contains last b-> true
+        | head::_ when Set.contains head b -> true
         | _::tail -> allExistsWithIn tail b
 
-    let reg : ClubRegister = [("Daniel", ("20167840", 1994, ["Gaming"; "Baseball"; "soccer"]));("Maja", ("12345678", 1991, ["ocult"; "antiphofeti"; "jazz"]));("Mor", ("321", 1981, ["ocult"; "soccer"; "jazz"]));("far", ("123", 1985, [ "jazz";"soccer"]))]
+    let reg : ClubRegister = Map.ofList [("Daniel", ("20167840", 1994, Set.ofList ["Gaming"; "Baseball"; "soccer"]));("Maja", ("12345678", 1991, Set.ofList ["ocult"; "antiphofeti"; "jazz"]));("Mor", ("321", 1981, Set.ofList ["ocult"; "soccer"; "jazz"]));("far", ("123", 1985, Set.ofList [ "jazz";"soccer"]))]
     let p1 : Arrangement = ((fun _ -> true), (fun by -> by>1982), (fun intersts -> allExistsWithIn ["jazz";"soccer"] intersts ))
     let p2 : Arrangement = ((fun _ -> true), (fun by -> by>1982), (fun intersts -> oneExistsWithIn ["jazz";"soccer"] intersts ))
     let p3 : Arrangement = ((fun _ -> true), (fun by -> by>1982), (fun intersts -> true))
@@ -35,16 +35,17 @@ module club =
         (telPredicate telephoneNumber) && (birthPredicate birth) && (interestsPredicate intersts)
 
 
+    let extractTargetGroupL5 p r = List.map (fun (name, (telephone,_,_)) -> (name,telephone)) (Map.toList (Map.filter (fun _ description -> isMemberPartOfTargetArrangement description p) r))
 
-    let rec extractTargetGroup p r = 
-       match r : ClubRegister with 
-       | [] -> []
-       | (name,description)::tail when isMemberPartOfTargetArrangement description p -> 
-            let (telephoneNumber, _, _) = description
-            (name,telephoneNumber)::extractTargetGroup p tail
-       | _ -> []
+    //let rec extractTargetGroup p r = 
+    //   match r : ClubRegister with 
+    //   | r when Map.isEmpty r -> []
+    //   | r when Map.filter (name,description)::tail when isMemberPartOfTargetArrangement description p -> 
+    //        let (telephoneNumber, _, _) = description
+    //        (name,telephoneNumber)::extractTargetGroup p tail
+    //   | _ -> []
 
     let test =
-        printfn "Test Arrangement 1: %A" (extractTargetGroup p1 reg)
-        printfn "Test Arrangement 2: %A" (extractTargetGroup p2 reg)
-        printfn "Test Arrangement 3: %A" (extractTargetGroup p3 reg)
+        printfn "Test Arrangement 1: %A" (extractTargetGroupL5 p1 reg)
+        printfn "Test Arrangement 2: %A" (extractTargetGroupL5 p2 reg)
+        printfn "Test Arrangement 3: %A" (extractTargetGroupL5 p3 reg)

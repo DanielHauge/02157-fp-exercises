23314030ec034e12a3d889bc6fe32fb157f7c612
D. Hauge <Animcuil@gmail.com>
2020-10-27 11:38:35 +0100

    Compiler exercise part 1

---
+++ exercises/compiler-exercise/Library.fs
@@ -0,0 +1,45 @@
+﻿namespace compiler_exercise
+
+module Compiler =
+    type Instruction = | ADD | SUB | SIGN | ABS | PUSH of int
+    type Stack = int list
+
+    let intpInstr (s:Stack) (i:Instruction) : Stack = 
+
+        let add (xs:Stack) : Stack =
+            match xs with 
+            | [] -> []
+            | head::tail when not tail.IsEmpty -> (head + tail.Head)::tail.Tail
+            | _::tail when tail.IsEmpty -> failwith "tail was empty, only 1 value"
+            | _ -> []
+
+        let sub xs =
+            match xs with 
+            | [] -> []
+            | head::tail when not tail.IsEmpty ->  (head - tail.Head)::tail.Tail
+            | _::tail when tail.IsEmpty -> failwith "tail was empty, only 1 value"
+
+        let abs xs =
+            match xs with
+            | [] -> []
+            | head::tail when head < 0 -> -head::tail
+            | noChange -> noChange 
+
+        let sign xs =
+            match xs with
+            | [] -> []
+            | head::tail -> -head::tail
+
+        let interpret =
+            function
+            | ADD -> add s
+            | SUB -> sub s
+            | SIGN -> sign s
+            | ABS -> abs s
+            | PUSH n -> n::s 
+            | _ -> s
+
+        interpret i
+
+    let exec (ins:Instruction list) : int = (List.fold (fun a -> intpInstr a) [] ins).Head
+        
